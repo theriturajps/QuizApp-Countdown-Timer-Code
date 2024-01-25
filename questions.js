621385b81c116e74c49f81d2cb9bedcb5ad45cc6
RituRajPS <theriturajps@gmail.com>
2024-01-25 18:27:01 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+  numb: 1,
+  question: "What is 2+2?",
+  answer: "4",
+  options: [
+    "-2",
+    "5",
+    "4",
+    "1"
+  ]
+},
+{
+  numb: 2,
+  question: "Who are you?",
+  answer: "Human",
+  options: [
+    "Animal",
+    "Human",
+    "Male",
+    "Female"
+  ]
+},
+{
+  numb: 3,
+  question: "Where do you find god?",
+  answer: "In Parents",
+  options: [
+    "In Statue",
+    "In Temple",
+    "In Everyone",
+    "In Parents"
+  ]
+},
+{
+  numb: 4,
+  question: "What is the correct website domain you find this Quiz Code?",
+  answer: "www.riturajps.in",
+  options: [
+    "www.google.in",
+    "www.riturajps.com",
+    "t.me/riturajps",
+    "www.riturajps.in"
+  ]
+},
+{
+  numb: 5,
+  question: "What does Three Zero Four Four Means?",
+  answer: "Can't Say",
+  options: [
+    "0004444",
+    "3044",
+    "3,0,4,4",
+    "Can't Say"
+  ]
+},
+];
